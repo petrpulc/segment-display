0480aedaa2303b4b9cb929d9d5ac03abe83235a0
Petr Pulc <pulcpetr@fit.cvut.cz>
2017-07-27 13:47:36 +0200

    Correct intenting and spacing + GPIO is constant

--- display.py
+++ display.py
@@ -1,7 +1,8 @@
 import time
-import RPi.GPIO as gpio
 
-gpio.setmode(gpio.BOARD)
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
 
 SEGMENTS = [18,16,12,10,3,8,5,7]
 NUMBERS = [19,15,13,11]
@@ -9,27 +10,30 @@ CHARS = {' ': (1,1,1,1,1,1,1,1),
          '0': (0,0,0,1,0,0,0,1)}
 
 def init():
-  for p in SEGMENTS:
-    gpio.setup(p, gpio.OUT, initial=1)
-  for p in NUMBERS:
-    gpio.setup(p, gpio.OUT, initial=0)
+    for p in SEGMENTS:
+        GPIO.setup(p, GPIO.OUT, initial=1)
+    for p in NUMBERS:
+        GPIO.setup(p, GPIO.OUT, initial=0)
+
 
 def out_zip(pins, vals):
-  for p, v in zip(pins,vals):
-    gpio.output(p, v)
+    for p, v in zip(pins, vals):
+        GPIO.output(p, v)
+
 
 def clear():
-  for p in SEGMENTS:
-    gpio.output(p, 1)
-  for p in NUMBERS:
-    gpio.output(p, 0)
+    for p in SEGMENTS:
+        GPIO.output(p, 1)
+    for p in NUMBERS:
+        GPIO.output(p, 0)
 
-def show(text,duration=1):
-  for _ in range(0, duration*50):
-    number = 0
-    for char in text[:4]:
-      out_zip(SEGMENTS, CHARS.get(char, (1,0,0,0,0,0,0,1)))
-      gpio.output(NUMBERS[number], 1)
-      time.sleep(0.004)
-      gpio.output(NUMBERS[number], 0)
-      number += 1
+
+def show(text, duration=1):
+    for _ in range(0, duration * 50):
+        number = 0
+        for char in text[:4]:
+            out_zip(SEGMENTS, CHARS.get(char, (1, 0, 0, 0, 0, 0, 0, 1)))
+            GPIO.output(NUMBERS[number], 1)
+            time.sleep(0.004)
+            GPIO.output(NUMBERS[number], 0)
+            number += 1
